fdbffcf8c30549e969710ef081866062e46be51f
jkampich1411 <jakob.kampichler@gmail.com>
2023-11-28 20:41:54 +0100

    updated dec4iot app from personal repo

--- onboarding-android/app/src/main/java/run/jkdev/dec4iot/jetpack/BangleJsDataReceiver.kt
+++ onboarding-android/app/src/main/java/run/jkdev/dec4iot/jetpack/BangleJsDataReceiver.kt
@@ -1,12 +1,15 @@
 package run.jkdev.dec4iot.jetpack
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Build
+import android.os.SystemClock
+import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.work.*
@@ -15,15 +18,26 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.Granularity
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.gson.Gson
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import run.jkdev.dec4iot.jetpack.enums.LocationType
+import run.jkdev.dec4iot.jetpack.gms.CellGeoloc
+import run.jkdev.dec4iot.jetpack.gms.Result
 import run.jkdev.dec4iot.jetpack.gsonmodels.BangleJsSendData
 import run.jkdev.dec4iot.jetpack.gsonmodels.BangleJsSendDataData
 import run.jkdev.dec4iot.jetpack.gsonmodels.BangleJsSendDataInfo
+import run.jkdev.dec4iot.jetpack.gsonmodels.CellInfo
+import run.jkdev.dec4iot.jetpack.gsonmodels.MLSResponse
+import run.jkdev.dec4iot.jetpack.gsonmodels.NetworkInfo
 import run.jkdev.dec4iot.jetpack.gsonmodels.SenmlField
 import java.io.IOException
 import java.time.Instant
@@ -34,6 +48,9 @@ class BangleJsDataReceiver : BroadcastReceiver() {
     private val gson = Gson()
 
     private lateinit var locationClient: FusedLocationProviderClient
+    private lateinit var telephonyManager: TelephonyManager
+    private lateinit var cellGeoloc: CellGeoloc
+
     private lateinit var locationTask: Task<Location>
 
     private lateinit var info: BangleJsSendDataInfo
@@ -45,6 +62,8 @@ class BangleJsDataReceiver : BroadcastReceiver() {
         if(context == null || intent == null) { return }
 
         if(intent.action === "me.byjkdev.dec4iot.intents.banglejs.SEND_DATA") {
+            Log.i(TAG, "Intent received!!!!")
+
             val data = intent.getStringExtra("json_data")
             val jsonData: BangleJsSendData = gson.fromJson(data, BangleJsSendData::class.java)
 
@@ -52,15 +71,13 @@ class BangleJsDataReceiver : BroadcastReceiver() {
             sensors = jsonData.data
 
             locationClient = LocationServices.getFusedLocationProviderClient(context)
+            telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            cellGeoloc = CellGeoloc(telephonyManager)
 
             this.context = context
 
-            if (checkSelfPermission(context, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return
-            }
-
             idField = SenmlField(
-                base_name = "urn:dev:mac:${formatMacAddress(info.mac_address)}",
+                base_name = "urn:dev:mac:${formatMacAddress(info.mac_address)}:",
                 base_time = getCurrentEpochSecond(),
 
                 name = "identifier",
@@ -148,8 +165,10 @@ class BangleJsDataReceiver : BroadcastReceiver() {
             val jsonObject = gson.toJson(fields)
 
             val inputData = Data.Builder()
-                .putString("endpoint", "https://www.${info.sensor_endpoint}")
+                .putString("endpoint", "https://${info.sensor_endpoint}")
                 .putString("json", jsonObject.toString())
+                .putString("bangleMessage", "Your emergency call succeeded.")
+                .putBoolean("shouldMessageBangle", true)
                 .build()
 
             val workRequest: WorkRequest =
@@ -158,50 +177,102 @@ class BangleJsDataReceiver : BroadcastReceiver() {
                     .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
                     .build()
 
-            WorkManager
+            val operation = WorkManager
                 .getInstance(this@BangleJsDataReceiver.context)
                 .enqueue(workRequest)
 
-            val locationRequest = CurrentLocationRequest.Builder()
-                .setGranularity(Granularity.GRANULARITY_FINE)
-                .setMaxUpdateAgeMillis(0)
-                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
-                .setDurationMillis(Long.MAX_VALUE)
-                .build()
+            val workInfo = WorkManager
+                .getInstance(this@BangleJsDataReceiver.context)
+                .getWorkInfoById(workRequest.id)
 
+            val workInfoListener = Runnable {
+                val info = workInfo.get()
+                Log.i(TAG, info.state.name)
 
-            locationTask = locationClient.getCurrentLocation(locationRequest, null)
-            locationTask.addOnFailureListener {
+            }
+
+            operation.result.addListener(workInfoListener, context.mainExecutor)
+
+            // MLS Location is ALWAYS sent
+            val network = cellGeoloc.getNetworkInfo()
+            val cells = cellGeoloc.getCells()
+
+            mlsLocationRequest(network, cells)
+
+            // Live Location allowed?
+            if (checkSelfPermission(context, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                val locationRequest = CurrentLocationRequest.Builder()
+                    .setGranularity(Granularity.GRANULARITY_FINE)
+                    .setMaxUpdateAgeMillis(0)
+                    .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                    .setDurationMillis(Long.MAX_VALUE)
+                    .build()
+
+                locationTask = locationClient.getCurrentLocation(locationRequest, null)
+                locationTask.addOnFailureListener {
+                    locationTask = locationClient.lastLocation
+                    locationTask.addOnSuccessListener(lastLocationOnSuccessListener)
+                }
+                locationTask.addOnSuccessListener(locationOnSuccessListener)
+            } else {
+                // If no Live Location Permission, then try LastLocation; This doesn't require Live Location Perm
                 locationTask = locationClient.lastLocation
                 locationTask.addOnSuccessListener(lastLocationOnSuccessListener)
             }
-            locationTask.addOnSuccessListener(locationOnSuccessListener)
-
         }
     }
 
     @SuppressLint("MissingPermission")
-    private val locationOnSuccessListener = OnSuccessListener<Location> {
+    private val locationOnSuccessListener = OnSuccessListener<Location?> {
         if (it == null) {
             locationTask = locationClient.lastLocation
             locationTask.addOnSuccessListener(lastLocationOnSuccessListener)
             return@OnSuccessListener
         }
 
-        latLongFieldsArrived(it.latitude, it.longitude)
+        if(Build.VERSION.SDK_INT >= 33) {
+            latLongFieldsArrived(it.latitude, it.longitude, it.accuracy, it.elapsedRealtimeAgeMillis, LocationType.GPS_CURRENT_LOCATION)
+        } else {
+            val nsElapsedSinceSysBoot = SystemClock.elapsedRealtimeNanos()
+            val nsElapsedSinceLoc = it.elapsedRealtimeNanos
+            val msLocationAge = (nsElapsedSinceSysBoot - nsElapsedSinceLoc) / 1000000
 
-    }
-
-    private val lastLocationOnSuccessListener = OnSuccessListener<Location> {
-        if(it == null) {
-            latLongFieldsArrived(0.0, 0.0)
-            return@OnSuccessListener
+            latLongFieldsArrived(it.latitude, it.longitude, it.accuracy, msLocationAge, LocationType.GPS_CURRENT_LOCATION)
         }
 
-        latLongFieldsArrived(it.latitude, it.longitude)
     }
 
-    private fun latLongFieldsArrived(lat: Number, long: Number) {
+    private val lastLocationOnSuccessListener = OnSuccessListener<Location?> {
+        if(it == null) return@OnSuccessListener
+        if(Build.VERSION.SDK_INT >= 33) {
+            latLongFieldsArrived(it.latitude, it.longitude, it.accuracy, it.elapsedRealtimeAgeMillis, LocationType.GPS_LAST_LOCATION)
+        } else {
+            val nsElapsedSinceSysBoot = SystemClock.elapsedRealtimeNanos()
+            val nsElapsedSinceLoc = it.elapsedRealtimeNanos
+            val msLocationAge = (nsElapsedSinceSysBoot - nsElapsedSinceLoc) / 1000000
+
+            latLongFieldsArrived(it.latitude, it.longitude, it.accuracy, msLocationAge, LocationType.GPS_LAST_LOCATION)
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun mlsLocationRequest(networkInfo: NetworkInfo, cells: List<CellInfo>) {
+        if (
+            checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) return
+
+        GlobalScope.launch {
+            when(val result = cellGeoloc.requestGeolocation(networkInfo, cells)) {
+                is Result.Success<MLSResponse> -> latLongFieldsArrived(result.data.location.latitude, result.data.location.longitude, result.data.accuracy, 1, LocationType.MLS_LOCATION_VIA_CELL_TOWERS)
+                is Result.Error -> {
+                    Log.e(TAG, "MLS request threw", result.exception)
+                }
+            }
+        }
+    }
+
+    private fun latLongFieldsArrived(lat: Number, long: Number, accuracy: Number, msElapsedSince: Number, source: LocationType) {
         val idFieldNew = idField
         idFieldNew.base_time = getCurrentEpochSecond()
 
@@ -216,83 +287,107 @@ class BangleJsDataReceiver : BroadcastReceiver() {
             SenmlField(
                 name = "longitude",
                 value = long,
-                unit = "long"
+                unit = "lon"
+            ),
+
+            SenmlField(
+                name = "accuracy",
+                value = accuracy,
+                unit = "m"
+            ),
+
+            SenmlField(
+                name = "timestamp",
+                value = msElapsedSince.toFloat() / 1000.toFloat(),
+                unit = "s"
+            ),
+
+            SenmlField(
+                name = "source",
+                value = source.toInt()
             )
         )
 
         val jsonObject = gson.toJson(fields)
 
         val inputData = Data.Builder()
-            .putString("endpoint", "https://www.${info.sensor_endpoint}")
+            .putString("endpoint", "https://${info.sensor_endpoint}")
             .putString("json", jsonObject.toString())
             .build()
 
-        val workRequest: WorkRequest =
+        val workRequest: OneTimeWorkRequest =
             OneTimeWorkRequestBuilder<UploadWorker>()
                 .setInputData(inputData)
                 .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
                 .build()
+
 
         WorkManager
             .getInstance(this@BangleJsDataReceiver.context)
             .enqueue(workRequest)
     }
 
-    class UploadWorker(appContext: Context, workerParams: WorkerParameters) : Worker(appContext, workerParams) {
+    class UploadWorker(private val appContext: Context, workerParams: WorkerParameters) : CoroutineWorker(appContext, workerParams) {
         private val okHttpClient = OkHttpClient()
+        private var shouldMessageBangle = false
+        private var bangleMessage: String? = ""
 
-        override fun getForegroundInfo(): ForegroundInfo {
+        private fun sendMessageBox(title: String, message: String) {
+            val sendMsgIntent = Intent("com.banglejs.uart.tx")
+            sendMsgIntent.putExtra("line", "E.showMessage(\"$message\", \"$title\");Bangle.buzz(1000, 100)")
+
+            appContext.sendBroadcast(sendMsgIntent)
+        }
+
+        override suspend fun getForegroundInfo(): ForegroundInfo {
             Log.i(TAG, "Foreground Job Started for UploadWorker")
             return super.getForegroundInfo()
         }
 
-        override fun doWork(): Result {
+        override suspend fun doWork(): Result {
+            this.shouldMessageBangle = inputData.getBoolean("shouldMessageBangle", false)
+            this.bangleMessage = inputData.getString("bangleMessage")
+
             val request = Request.Builder()
                 .tag(TAG)
                 .url(inputData.getString("endpoint")!!)
                 .post(inputData.getString("json")!!.toRequestBody("application/json".toMediaType()))
                 .build()
 
-            val http = sendHttpRequest(request)
-            return if(http is Result) {
-                http
-            } else {
-                Result.success()
-            }
+            return sendHttpRequest(request)
         }
 
-        private fun sendHttpRequest(request: Request): Result? {
-            try {
-                okHttpClient.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        Log.e(TAG, "HTTP Request threw:\n${e.stackTrace}")
-                        throw Exception("HTTP Request threw", e)
+        private suspend fun sendHttpRequest(request: Request): Result {
+            return withContext(Dispatchers.IO) {
+                try {
+                    val response = okHttpClient.newCall(request).execute()
+                    val workResultData = workDataOf(
+                        Pair("code", response.code),
+                        Pair("response", response.body!!.string())
+                    )
+                    response.close()
+
+                    if(response.code in 200..299) {
+                        if (shouldMessageBangle && bangleMessage != null) sendMessageBox("", bangleMessage!!)
+                        return@withContext Result.success(workResultData)
+                    } else {
+                        sendMessageBox("FUCK", "A request failed, please call emergency services from your phone!!")
+                        return@withContext Result.failure(workResultData)
                     }
 
-                    override fun onResponse(call: Call, response: Response) {
-                        Log.i(TAG, "HTTP Request returned response:\n${response}")
-                        Log.i(TAG, "type: ${response.request.method}")
-                        Log.i(TAG, "code: ${response.code}")
-                        Log.i(TAG, "headers: ${response.headers}")
-                        Log.i(TAG, "data ${response.body.toString()}")
-                        response.close()
-                    }
-
-                })
-            } catch (e: Exception) {
-                return Result.failure()
+                } catch (e: IOException) {
+                    Log.e(TAG, "HTTP Request threw", e)
+                    return@withContext Result.failure(
+                        workDataOf(Pair("Exception", e))
+                    )
+                }
             }
-
-            return null
         }
-
     }
 }
 
-fun getCurrentEpochSecond(): String {
-    val epochSecond = Instant.now().epochSecond.toDouble()
-
-    return "%1.9e".format(epochSecond)
+fun getCurrentEpochSecond(): Double {
+    return Instant.now().epochSecond.toDouble()
 }
 
 fun formatMacAddress(macAddress: String): String {
